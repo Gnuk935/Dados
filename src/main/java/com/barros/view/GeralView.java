279e0d852516c8a4f0083e6b460e17fd2ec41d5c
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2023-01-19 18:59:15 -0300

    Incial

---
+++ src/main/java/com/barros/view/GeralView.java
@@ -0,0 +1,10 @@
+package com.barros.view;
+
+public class GeralView {
+	public void Inicio() {
+		System.out.println("===========================================================");
+		System.out.println("Esta é uma versão beta de um sistema de geração de requerimentos");
+		System.out.println("Tem como objetivo ser integrado ao SDM");
+		System.out.println("===========================================================");
+	}
+}
